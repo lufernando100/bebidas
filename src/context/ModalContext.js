de90b00636dc5c77fdae672fe5f6087db1436747
Luis Fernando Hernandez Fernandez <lhernandez@psl.com.co>
2020-07-01 21:59:03 -0500

    Add context and other components.

---
+++ src/context/ModalContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+// Crear el context
+
+export const ModalContext = createContext();
+
+const ModalProvider = (props) => {
+  //State del provider
+  const [idreceta, guardarIdReceta] = useState(null);
+  const [info, guardarReceta] = useState({});
+
+  // Una vez tenemos la receta, llamar la API
+
+  useEffect(() => {
+    const obtenerReceta = async () => {
+      if (!idreceta) return;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+      const respuesta = await axios(url);
+      guardarReceta(respuesta.data.drinks[0]);
+    };
+    obtenerReceta();
+  }, [idreceta]);
+
+  return (
+    <ModalContext.Provider
+      value={{
+        info,
+        guardarIdReceta
+      }}
+    >
+      {props.children}
+    </ModalContext.Provider>
+  );
+};
+
+export default ModalProvider;
